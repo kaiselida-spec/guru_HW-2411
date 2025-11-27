c9699d9d64453d06ffd3d0f3e78396a0c433538f
Elida <kaiselida@gmail.com>
2025-11-27 16:30:20 +0600

    Update Main.java

--- src/main/java/com/elidakaiis/Main.java
+++ src/main/java/com/elidakaiis/Main.java
@@ -3,7 +3,7 @@ package com.elidakaiis;
 public class Main {
     public static void main(String[] args) {
 
-        static private int number; 
+        int number = 0; 
         System.out.println("Hello and welcome!");
 
         System.out.println("Aruuke is the best");
