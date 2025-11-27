d28ba358cf4d619f1e7dc8a15a1de0d429b65f03
Elida <kaiselida@gmail.com>
2025-11-27 15:36:48 +0600

    Update Main.java

--- src/main/java/com/elidakaiis/Main.java
+++ src/main/java/com/elidakaiis/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
+        System.out.println("Aruuke is the best");
+
 
         }
         private static String getMessage() {
-        return "Helllo qa.guru";
+        return "Helllo qa.guru YESSS";
+            
         }
     }
 
